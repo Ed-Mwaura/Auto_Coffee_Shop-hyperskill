82e99e9cb22f639ea6f92ade0c2025a602b4ad71
Edwin Mwaura <mwauraedwin97@gmail.com>
2020-06-17 15:32:21 +0300

    Second commit, fully automated shop

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+# The global variables
 water = 400
 milk = 540
 coffee = 120
@@ -35,7 +38,7 @@ def fill():
     new_cups = int(input())
     cups += new_cups
 
-    const_texts()
+    # const_texts()
 
 
 def take():
@@ -43,51 +46,94 @@ def take():
     print("I gave you $", money)
     money = 0
     print()
-    const_texts()
+    # const_texts()
 
 
 def buy():
-    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-    option = int(input())
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+    option = input()
     global water
     global milk
     global coffee
     global money
     global cups
-    if option == 1:
-        water -= 250
-        coffee -= 16
-        money += 4
-        cups -= 1
-    elif option == 2:
-        water -= 350
-        milk -= 75
-        coffee -= 20
-        money += 7
-        cups -= 1
-    elif option == 3:
-        water -= 200
-        milk -= 100
-        coffee -= 12
-        money += 6
-        cups -= 1
+    if option == "1":
+
+        if water - 250 < 0:
+            print("Sorry, not enough water!")
+        elif coffee - 16 < 0:
+            print("Sorry, not enough coffee!")
+        elif cups - 1 < 0:
+            print("Sorry, not enough cups!")
+        else:
+            water -= 250
+            coffee -= 16
+            money += 4
+            cups -= 1
+            print("I have enough resources, making you a coffee!")
+    elif option == "2":
+
+        if water - 350 < 0:
+            print("Sorry, not enough water!")
+        elif coffee - 20 < 0:
+            print("Sorry, not enough coffee!")
+        elif milk - 75 < 0:
+            print("Sorry, not enough milk!")
+        elif cups - 1 < 0:
+            print("Sorry, not enough cups!")
+        else:
+            water -= 350
+            milk -= 75
+            coffee -= 20
+            money += 7
+            cups -= 1
+            print("I have enough resources, making you a coffee!")
+
+    elif option == "3":
+
+        if water - 200 < 0:
+            print("Sorry, not enough water!")
+        elif coffee - 12 < 0:
+            print("Sorry, not enough coffee!")
+        elif milk - 100 < 0:
+            print("Sorry, not enough milk!")
+        elif cups - 1 < 0:
+            print("Sorry, not enough cups!")
+        else:
+            water -= 200
+            milk -= 100
+            coffee -= 12
+            money += 6
+            cups -= 1
+            print("I have enough resources, making you a coffee!")
+
+    elif option == 'back':
+        intro()
     else:
         print("Invalid option")
-    const_texts()
+    # const_texts()
 
 
 def intro():
-    const_texts()
-    print()
-    print("Write action (buy, fill, take):")
-    choice = input()
+    while True:
+        # const_texts()
+        print()
+        print("Write action (buy, fill, take, remaining, exit):")
+        choice = input()
 
-    if choice == 'buy':
-        buy()
-    elif choice == 'fill':
-        fill()
-    elif choice == 'take':
-        take()
+        if choice == 'buy':
+            buy()
+        elif choice == 'fill':
+            fill()
+        elif choice == 'take':
+            take()
+        elif choice == 'remaining':
+            const_texts()
+        elif choice == 'exit':
+            sys.exit()
+        else:
+            print("Invalid choice. Try again")
+            intro()
 
 
 intro()
